6e5d2528b2426dee9bdc7fb975e8e07c554cf103
João Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2023-02-11 02:14:13 -0300

    until about section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const menuButton = document.getElementById('menu-btn');
 const navbar = document.getElementById('navbar');
+const test2 = document.querySelectorAll('.about .video-container .controls .control-btn');
 
 menuButton.onclick = () => {
     navbar.classList.toggle('active')
 }
+
+test2.forEach( btn => {
+    btn.onclick = () => {
+        console.log('aa')
+    }
+})
