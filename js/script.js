709062ce7c24568fe42dd7eceb8d5564b4055094
João Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2023-02-13 18:20:24 -0300

    footer save

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 const menuButton = document.getElementById('menu-btn');
 const navbar = document.getElementById('navbar');
-const test2 = document.querySelectorAll('.about .video-container .controls .control-btn');
+const videoControlButtons = document.querySelectorAll('.about .video-container .controls .control-btn');
+const video = document.querySelector('.about .video-container .video');
 
 menuButton.onclick = () => {
     navbar.classList.toggle('active')
 }
 
-test2.forEach( btn => {
-    btn.onclick = () => {
-        console.log('aa')
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+videoControlButtons.forEach(button => {
+    button.onclick = () => {
+        let src = button.getAttribute('data-src');
+        video.setAttribute('src', src);
     }
 })
